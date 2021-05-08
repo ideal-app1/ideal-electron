9b280d73bc85ad8e22e589c391a0015f90e0f4de
lantoine <antoine.leroux@epitech.eu>
2021-05-08 21:44:38 +0200

    Send widgetId for Axel and implementation Save function Codelink
    Former-commit-id: dacd971d76bf976c1bd17ea7cf0136a71b741362

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
                     <Route exact path="/">
                         <Main/>
                     </Route>
-                    <Route exact path="/codelink/:filepath" component={CodeLink}/>
+                    <Route exact path="/codelink/:filepath/:idwidget" component={CodeLink}/>
                 </Switch>
             </Router>
             /*  */
