3891f59d67bc1fbbe44872f2fa7203cbb000ab4a
Axel Ambert <axel.ambert@epitech.eu>
2021-05-15 15:27:37 +0100

    Add new version of CodeLinkNodeLoader
    Former-commit-id: 2af74e86376d374cab2ee12f9854d293a80d49c6

--- src/Components/CodeLink/CodeLinkNodeLoader.js
+++ src/Components/CodeLink/CodeLinkNodeLoader.js
@@ -2,30 +2,97 @@ import {LiteGraph} from "litegraph.js";
 import createMethodNode from "./CodeLinkNodes/MethodNode";
 import createFunctionNode from "./CodeLinkNodes/FunctionNode";
 import createValue from "./CodeLinkNodes/ConstValueNode";
+import createClassNode from "./CodeLinkNodes/ClassNode";
+import createConstructorAttributeNode from "./CodeLinkNodes/ConstructorAttributeNode";
 
-let graph = null;
+let LCanvas = null;
+
+const getConstructor = (widget, data) => {
+    const widgetData = data.find(classData => {
+        return (classData["name"] === widget);
+
+    });
+
+
+    if (!widgetData) {
+        console.log("Error - The class was not found.");
+        //TODO implement an error when the widget is not found.
+        return;
+    }
+    return (widgetData['constructors'].find(constructorData => constructorData['isMainConstructor'] === "true"));
+
+}
+
+const createMainWidget = (className) => {
+    console.log("THE LENGTH");
+    const mainWidget = LiteGraph.createNode("Classes/" + className);
+    mainWidget.pos = [250, 250];
+    LCanvas.graph.add(mainWidget);
+}
+
+
+const createAttributes = (widget, constructor, requiredOnly = true) => {
+
+    const pos = [500, 300];
+
+    constructor.parameters.forEach((constructorParameter) => {
+        const tmpPos = [pos[0], pos[1]];
+        if (requiredOnly === true && constructorParameter.isRequired !== "true") {
+            console.log("NoN.");
+            console.log(constructorParameter)
+            return;
+        }
+        console.log("j'invoque " + widget + " constructor's attributes/" + constructorParameter["name"])
+        const attribute = LiteGraph.createNode(widget + " constructor's attributes/" + constructorParameter["name"]);
+        attribute.pos = tmpPos;
+        LCanvas.graph.add(attribute);
+        console.log("size ? ")
+        console.log(attribute.size)
+        pos[1] += attribute.size[1] + 50;
+    })
+}
 
 const CodeLinkNodeLoader = {
-    registerGraph: graphToRegister => {
-      graph = graphToRegister;
+    registerLCanvas: canvasToRegister => {
+      LCanvas = canvasToRegister;
     },
-    loadEveryKnownNodes: (err, data) => {
-        const parsed = JSON.parse(data);
+    loadEveryKnownNodes: (parsed) => {
+
+        const constructor = getConstructor('TextButton', parsed["classes"]);
 
         parsed["funcs"].forEach((data) => {
-            createFunctionNode(data, graph);
+            createFunctionNode(data, LCanvas);
         });
         parsed["constValues"].forEach((data) => {
-            createValue(data, graph);
+            createValue(data, LCanvas);
         });
+        if (constructor) {
+            constructor["parameters"].forEach((constructorParameter) => {
+                createConstructorAttributeNode('TextButton', constructorParameter, LCanvas);
+            })
+        }
+
         parsed["classes"].forEach((classObject) => {
-            classObject["methods"].forEach((value) => {
-                createMethodNode(value, classObject["name"], graph);
-            });
+            if (classObject["name"] === "TextButton") {
+                createClassNode(classObject, true, LCanvas);
+                classObject["methods"].forEach((value) => {
+                    createMethodNode(value, classObject["name"], LCanvas);
+                });
+            }
         });
-
     },
+    addMainWidgetToView: (widget, data) => {
+        console.log("Im called")
+        const constructor = getConstructor(widget, data);
 
+        if (!constructor) {
+            console.log("Error - The constructor was not found.");
+            //TODO implement an error when the main constructor is not found.
+            return;
+        }
+        createMainWidget(widget);
+        createAttributes(widget, constructor);
+    }
 }
 
 
